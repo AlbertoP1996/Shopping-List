c64857f88d72c7b70992c114444295cd39b89f1f
Alberto Postiglione <alberto.postiglione.96@gmail.com>
2024-05-18 11:02:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Tabs from "./components/Tabs.vue";
+</script>
+
+<template>
+    <Tabs/>
+</template>
+
+
---
+++ src/components/Tabs.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+let tab = ref(1);
+import Home from "./Home.vue";
+import ProductsToBuy from "./ProductsToBuy.vue";
+
+</script>
+
+<template>
+  <div class="tabs w-full">
+  <!-- Tabs -->
+    <div class="flex justify-center p-5">
+      <button :class="{'btn-neutral' : tab === 1 }" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="tab=1">Home</button>
+      <button :class="{'btn-neutral' : tab === 2 }" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="tab=2">Prodotti da acquistare</button>
+    </div>
+    <!-- Contenuto -->
+    <div class="contents">
+      <div v-if="tab == 1" class="py-10 px-6">
+      <Home/>
+      </div>
+      <div v-if="tab == 2" class="py-10 px-6">
+      <ProductsToBuy/>
+      </div>
+    </div>
+  </div>
+</template>
+
+
