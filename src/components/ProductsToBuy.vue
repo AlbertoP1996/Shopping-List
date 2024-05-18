6dde6593f2671469c3d5d6c864a01c802d86d236
Alberto Postiglione <alberto.postiglione.96@gmail.com>
2024-05-18 12:07:05 +0200

    aggiunta elemento alla lista spesa

--- src/components/ProductsToBuy.vue
+++ src/components/ProductsToBuy.vue
@@ -1,15 +1,34 @@
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+let products = ref([]);
+
+onMounted(() =>{
+    axios.get('http://localhost:3000/products').then((res) =>{
+        // console.log(res.data);
+        products.value = res.data;
+    })
+})
+
+function onAddProduct(event){
+    console.log(event.target.value);
+}
+
+</script>
+
 <template>
 <!-- Sezione per aggiungere un nuovo prodotto -->
 <div class="w-full md:w-2/5 mx-auto my-4">
     <label class="input input-bordered flex items-center gap-2">
-        <input type="text" class="grow" placeholder="Aggiungi prodotto" />
+        <input type="text" @keyup.enter="onAddProduct($event)" class="grow" placeholder="Aggiungi prodotto" />
         <kbd class="kbd kbd-sm">Search</kbd>
     </label>
 </div>
 
     <!-- Lista dei prodotti -->
     <div class="w-full md:w-2/5 mx-auto border border-neutral border-lg">
-        <div class="flex justify-between">
+        <div v-for="product in products" :key="product.id" class="flex justify-between">
             <!-- Icona e nome prodotto -->
             <div class="flex items-center">
                 <button class="btn btn-square btn-ghost">
@@ -17,7 +36,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
                 </button>
-                <span class="label-text">Prodotto 1</span>    
+                <span class="label-text">{{product.name}}</span>    
             </div>
             
             <!-- Checkbox -->
